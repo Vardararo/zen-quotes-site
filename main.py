6ccdc7526978aeef5f7d7e89ae5aa695926e0aea
Edib H <edib.hafizovic@gmail.com>
2024-07-19 19:33:51 +0200

    Update index.html

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap5
+#from flask_bootstrap import Bootstrap5
 import requests
 from datetime import datetime
 
@@ -9,7 +9,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    pass
+    quote_response = requests.get("https://zenquotes.io/api/random")
+    qoute_data = quote_response.json()
+    
+    return render_template("index.html")
 
 
 if __name__ == '__main__':

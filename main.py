8e58c87eeedad7488479166226da13c6def32ad8
Edib H <edib.hafizovic@gmail.com>
2024-07-19 19:17:33 +0200

    Setup Flask server

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 import requests
 from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
